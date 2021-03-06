615c66e15dadb9063930adcadafb409c2d291f43
kdramoss <77633961+kdramoss@users.noreply.github.com>
2021-03-05 21:49:14 -0600

    Add files via upload

---
+++ Java_sccript/js/Ejemplo.js
@@ -0,0 +1,18 @@
+var titulo_html = document.getElementById('titulo')
+var caja_html = document.getElementById('caja')
+var boton_html = document.getElementById('boton')
+
+function sriptTitulo(){ 
+titulo_html.classList.remove("titulo");
+titulo_html.classList.add("titulo2");
+    
+};
+ 
+function sriptCaja(){ 
+caja_htm.classList.remove("caja");
+    caja_htm.classList.add("caja2");
+    
+    
+};
+
+boton_html.addEventListener('click');
---
+++ Java_sccript/js/Scrip.js
@@ -0,0 +1,17 @@
+var saludo = "Hola JavaScript"
+var edad = 16
+var unPoco = 1.5
+var siNo = true
+var seccion = 'a'
+
+console.log(saludo)
+
+edad = prompt("Edad")
+
+if(edad >= 18){
+   
+    alert("ares mayor de edad");
+    
+    }else{ 
+        alert("eres menor de edad");
+}
